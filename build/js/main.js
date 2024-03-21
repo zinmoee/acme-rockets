93832503127d632f6872b67536f5541a44572f7d
Zin Moe <dev.zinmoe@gmail.com>
2024-03-21 21:18:24 +0630

    create more sections and mobile nav

---
+++ build/js/main.js
@@ -0,0 +1,15 @@
+const initApp = () => {
+  const hamburgetBtn = document.getElementById("hamburger-button");
+
+  const mobileMenu = document.getElementById("mobile-menu");
+
+  const toggleMenu = () => {
+    mobileMenu.classList.toggle("hidden");
+    mobileMenu.classList.toggle("flex");
+  };
+
+  hamburgetBtn.addEventListener("click", toggleMenu);
+  mobileMenu.addEventListener("click", toggleMenu);
+};
+
+document.addEventListener("DOMContentLoaded", initApp);
